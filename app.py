2de14defeb88d872b4b99b93fc8774ba7082d8dc
MJ1717 <bananahamo77@gmail.com>
2025-04-08 17:19:23 -0400

    added api url on app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request
+import requests
 #render template allows to load html files
 #and the folder name has to be templates
 #request give access to data that user input (so artist name, song title)
 
+
 app = Flask(__name__)
+
+
 
 #when someone visit "/", run the function below
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
 
 #post is sending, user sending data to the server
 @app.route("/lyrics", methods=["POST"])
@@ -17,8 +23,25 @@ def get_lyrics():
     artist = request.form["artist"]
     title = request.form["title"]
 
-    #this is special, this shows on the web
-    return f"You searched for '{title}' by '{artist}'"
+    #make api url with artist and title
+    api_url = f"https://api.lyrics.ovh/v1/{artist}/{title}"
+
+    response = requests.get(api_url, headers={"User-Agent": "Mozilla/5.0"})
+
+    #convert it to json, (to dic), much easiwer to read
+    data = response.json()
+    #data have one key named 'lyrics' if there exist song that matches
+
+    if 'lyrics' in data:
+        lyrics = data['lyrics']
+    else:
+        lyrics = "No lyrics found with given data" 
+
+    return lyrics
+
+    
+
+
 
 #check if the script is running directly not by importing
 if __name__ == "__main__":
