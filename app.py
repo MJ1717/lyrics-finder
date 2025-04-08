3ba8a3bb2c58d61befd18ef5ef6af27c80494dfc
MJ1717 <bananahamo77@gmail.com>
2025-04-08 14:07:53 -0400

    added lyrics route

--- app.py
+++ app.py
@@ -10,6 +10,16 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+#post is sending, user sending data to the server
+@app.route("/lyrics", methods=["POST"])
+def get_lyrics():
+    # Get form input values
+    artist = request.form["artist"]
+    title = request.form["title"]
+
+    #this is special, this shows on the web
+    return f"You searched for '{title}' by '{artist}'"
+
 #check if the script is running directly not by importing
 if __name__ == "__main__":
     app.run(debug=True)
