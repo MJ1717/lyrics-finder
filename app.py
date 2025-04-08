b3244023688f3aec2b312d4c5503b6e5a65f2f8e
MJ1717 <bananahamo77@gmail.com>
2025-04-08 14:00:20 -0400

    updating

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
+#render template allows to load html files
+#and the folder name has to be templates
+#request give access to data that user input (so artist name, song title)
 
 app = Flask(__name__)
 
 #when someone visit "/", run the function below
 @app.route("/")
 def home():
-    return "Hello world"
+    return render_template("index.html")
 
 #check if the script is running directly not by importing
 if __name__ == "__main__":
